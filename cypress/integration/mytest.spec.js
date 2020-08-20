9fe6fdc89a33962d2e51c9f3e83ed3b3bc399769
Damian Obajtek <damian.obajtek@gmail.com>
2020-08-21 01:04:56 +0200

    positive

--- cypress/integration/mytest.spec.js
+++ cypress/integration/mytest.spec.js
@@ -1,8 +1,5 @@
 describe("Sample test", () => {
-  it("Search lollipop emoji", () => {
-    cy.visit("www.gdsiajgoioijoijo53125123i.com"); // launch project website
-    cy.get("input").type("lollipop"); // enter 'lollipop' in search
-    cy.get(".component-emoji-result-row").should("have.length", 1); // check if only 1 result is returned
-    cy.contains("Lollipop").should("have.class", "title"); // check if returned element contains 'title' class
+  it("Search google", () => {
+    cy.visit("www.google.com");
   });
 });
