40f73cafc82927a186415bcf94f0f4a2877e26ad
Damian Obajtek <damian.obajtek@gmail.com>
2020-08-21 12:11:51 +0200

    menu

--- cypress/integration/redesign/redesignpage.spec.js
+++ cypress/integration/redesign/redesignpage.spec.js
@@ -8,6 +8,34 @@ describe("Czy strona redesign poprawnie sie laduja w ogole", () => {
   });
 });
 
+// describe("Czy nawigacja i menu poprawnie działa", () => {
+//   it("should open redesignPage", () => {
+//     cy.visit(redesignPage);
+//   });
+//   it("should check nav", () => {
+//     cy.get('[href="/register"] > .menu-item').should("contain", "Sign Up");
+//     cy.get('[href="/login"] > .menu-item').should("contain", "Login");
+//     cy.get(".ham").click();
+//     cy.get('[aria-current="page"] > li > .menu-item').should("contain", "Home");
+//     cy.get('[href="/movie"] > li > .menu-item').should("contain", "Movies");
+//     cy.get('[href="/tv"] > li > .menu-item').should("contain", "TV-Shows");
+//     cy.get('[href="/search"] > li > .menu-item').should("contain", "Search");
+//     cy.get('[href="/login"] > li > .menu-item').should("contain", "Login");
+//     cy.get('[href="/register"] > li > .menu-item').should("contain", "Sign Up");
+//   });
+// });
+
+describe("Czy menu otwiera się poprawnie, renderuje i zamyka", () => {
+  it("should open redesignPage", () => {
+    cy.visit(redesignPage);
+  });
+  it("should open menu, check and close", () => {
+    cy.get(".ham").click();
+    cy.wait(500);
+    cy.get(".ham").click();
+  });
+});
+
 describe("Czy podpowiedzi działają poprawnie", () => {
   it("should open basicPage", () => {
     cy.visit(redesignPage);
